5167266846c0a95f5d8804652f050554fdd0e71a
jheems <jheems@c88.seng.uvic.ca>
2016-04-02 11:00:47 -0700

    Add a working DHT, use rundht.m to do comparison

--- dht.m
+++ dht.m
@@ -1,28 +1,8 @@
-function d = dht(f)
-    [M N] = size(f);
-    d = zeros(size(f));
-    for i = 1:M
-        for j = 1:N
-            d(i,j) = H(f,i,j,M,N);
-        end
-    end
+function [ d ] = dht( f )
+% dht can be computed as the difference between the Re and Im parts of DFT.
+% Then it is scaled by the number of coefficients, which for 8x8 is 64.
+im2fft = fft2(f);
+d = (real(im2fft) - imag(im2fft))/64.0;
+
 end
-
-function c = cas (x)
-    c = cos(x) + sin(x);
-end
-
-function h = H(f, v1, v2, M, N)
-    entire_sum = 0;
-    for t1 = 1:M
-        inner_sum = 0.0;
-        for t2 = 1:N
-            inner_sum = f(t1, t2) * cas(2*pi*v1*t1/M) * cas(2*pi*v2*t2/N) + inner_sum;
-        end
-        entire_sum = entire_sum + inner_sum;
-    end
-    h = entire_sum/(M*N);
-end
-
-
 
--- rundht.m
+++ rundht.m
@@ -1,13 +1,15 @@
 % Question 9.8 from the text book
 
-function ex9_8
+
+
+function rundht
 
 cimage = input('Enter name of image: ','s');
 im = imread(cimage);
 
 
 % reconstruct with 4 coefficients
-[im_r,N,psnr] = coeff9_8(im,4);
+[im_r,N,psnr] = coeff(im,4);
 figure(1)
 imshow(uint8(im_r))
 title(sprintf('4 coefficients (PSNR = %.4f, %d nonzero coefficients in total)',...
@@ -15,7 +17,7 @@ title(sprintf('4 coefficients (PSNR = %.4f, %d nonzero coefficients in total)',.
 print -deps2 lena_4
 
 % reconstruct with 8 coefficients
-[im_r,N,psnr] = coeff9_8(im,8);
+[im_r,N,psnr] = coeff(im,8);
 figure(2)
 imshow(uint8(im_r))
 title(sprintf('8 coefficients (PSNR = %.4f, %d nonzero coefficients in total)',...
@@ -23,7 +25,7 @@ title(sprintf('8 coefficients (PSNR = %.4f, %d nonzero coefficients in total)',.
 print -deps2 lena_8
 
 % reconstruct with 16 coefficients
-[im_r,N,psnr] = coeff9_8(im,16);
+[im_r,N,psnr] = coeff(im,16);
 figure(3)
 imshow(uint8(im_r))
 title(sprintf('16 coefficients (PSNR = %.4f, %d nonzero coefficients in total)',...
@@ -31,7 +33,7 @@ title(sprintf('16 coefficients (PSNR = %.4f, %d nonzero coefficients in total)',
 print -deps2 lena_16
 
 % reconstruct with 32 coefficients
-[im_r,N,psnr] = coeff9_8(im,32);
+[im_r,N,psnr] = coeff(im,32);
 figure(4)
 imshow(uint8(im_r))
 title(sprintf('32 coefficients (PSNR = %.4f, %d nonzero coefficients in total)',...
@@ -46,11 +48,11 @@ print -deps2 lena_32
 %             N -- number of nonzero coefficients
 %          psnr -- psnr of the reconstructed image
 
-function [im_r,N,psnr] = coeff9_8(im,k)
+function [im_r,N,psnr] = coeff(im,k)
 
 im = double(im);
 
-im_c = blkproc(im,[8,8],'round(dht(x).*P1)',zigzag(8));
+im_c = blkproc(im,[8,8],'dht(x).*P1',coeff_indx(k));
 N = length(find(im_c ~= 0));
 im_r = blkproc(im_c,[8,8],'64.*dht(x)');
 psnr = 10*log10(255*255/mean(mean((im - im_r).^2)));
@@ -58,10 +60,33 @@ psnr = 10*log10(255*255/mean(mean((im - im_r).^2)));
 
 
 
-% vector a --> square matrix b (sizeb-by-sizeb) in zigzag order
-function b = zigzag(sizeb)
-dhtcoeff = [1 9 2 3 10 17 25 18];
-%dhtcoeff_2 = dhtcoeff(1:sizeb);
+% Compute a binary matrix following the scanning order of DHT
+function b = coeff_indx(sizeb)
+
+% shortcut to convert to linear index
+i = @(x,y) sub2ind([8 8], x, y);
+
+
+assert(sizeb <= 32);
+% only need the first 32 coeffs max
+%             1      2      3      4      5     6       7      8
+dhtcoeff = [i(1,1) i(1,2) i(2,1) i(1,3) i(3,1) i(1,4) i(4,1) i(1,5)
+%             9      10    11     12       13    14    15       16
+            i(5,1) i(1,6) i(6,1) i(1,7) i(7,1) i(1,8) i(8,1) i(2,2) 
+%             17     18    19     20      21     22      23      24
+            i(2,3) i(3,2) i(2,4) i(4,2) i(2,5) i(5,2)  i(2,6) i(6,2)
+%             25    26      27     28    29     30      31    32
+            i(2,7) i(7,2) i(2,8) i(8,2) i(3,3) i(3,4) i(4,3) i(3,5)];
+
+%truncate by how many we want to keep
+dhtcoeff_trunc = dhtcoeff(1:sizeb);
+
+% zeros everywehere
 b = zeros(8,8);
-b(dhtcoeff)=1;
+
+%save what we want to keep
+b(dhtcoeff_trunc)=1;
+
+
+
 
